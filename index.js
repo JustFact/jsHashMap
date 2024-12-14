baf5e95c448dfdffee1fb6a8a3c95ac321cc65fb
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-12-14 15:10:04 +0530

    Append new Hash key/value into bucket linkedList

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const newNode = (value) => {
+  return {
+    value: value ? value : null,
+    next: null,
+  };
+};
+
+const newLinkedList = () => {
+  let head = null;
+  const getHead = () => {
+    return head;
+  };
+
+  const append = (value) => {
+    if (head === null) {
+      head = newNode(value);
+      return;
+    } else {
+      let currentNode = head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode(value);
+    }
+  };
+
+  return {
+    append,
+    getHead,
+  };
+};
+
 function HashMap(capacity = 16) {
   let loadFactor = 0.75;
   let bucket = Array.from("_".repeat(capacity));
@@ -12,14 +44,33 @@ function HashMap(capacity = 16) {
     return hashCode;
   };
 
+  const set = (key, value) => {
+    let index = hash(key);
+    let list;
+    if (bucket[index] === "_") {
+      list = newLinkedList();
+      // console.log("call 1 ", list.getHead());
+    } else {
+      list = bucket[index];
+      // console.log("call 2", list.getHead());
+    }
+
+    list.append({ key, value });
+    bucket[index] = list;
+    // console.log("call 3 ", list.getHead());
+  };
+
   return {
     hash,
     bucket, //temporary access to bucket
+    set,
   };
 }
 
 let test = HashMap();
 
-console.log(test.hash("Abc"));
-console.log(test.hash("bAc"));
-console.log(test.hash("bcA"));
+console.log(test.bucket);
+test.set("Abc", "First");
+test.set("bAc", "Second");
+test.set("bcA", "Third");
+console.log(test.bucket);
